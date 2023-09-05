5341e77ec9f8ba786c54ddc6801beb112ad2092b
Cynthia Castillo <ca.castillo@gmail.com>
2023-09-05 16:16:24 -0600

    Vista index y nueva, crear2

---
+++ src/main/java/com/codingdojo/cynthia/controladores/Controlador.java
@@ -0,0 +1,100 @@
+package com.codingdojo.cynthia.controladores;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.codingdojo.cynthia.modelos.Etiqueta;
+import com.codingdojo.cynthia.modelos.Pregunta;
+import com.codingdojo.cynthia.servicios.Servicios;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class Controlador {
+	
+	@Autowired
+	private Servicios servicio;
+	
+	@GetMapping("/")
+	public String inicio() {
+		return "redirect:/preguntas";
+	}
+	
+	@GetMapping("/preguntas")
+	public String preguntas(Model model) {
+		List<Pregunta> preguntas = servicio.todasPreguntas();
+		model.addAttribute("preguntas", preguntas);
+		
+		return "index.jsp";
+	}
+	
+	@GetMapping("/nueva")
+	public String nueva(@ModelAttribute("pregunta") Pregunta pregunta) {
+		return "nueva.jsp";
+	}
+	
+	@PostMapping("/crear")
+	public String crear(@Valid @ModelAttribute("pregunta") Pregunta pregunta,
+						BindingResult result,
+						@RequestParam("textoEtiquetas") String textoEtiquetas) {
+		
+		if(result.hasErrors()) {
+			return "nueva.jsp";
+		} else {
+			//textoEtiquetas = "java, programacion, tecnología"
+			//listaEtiquetas = {"java", "programacion", "tecnología"}
+			//trim() -> quitar los espacios alrededor
+			//split() -> dividir el texto en base a un caracter
+			String[] listaEtiquetas = textoEtiquetas.trim().split(",");
+			List<Etiqueta> etiquetas = new ArrayList<>(); // Lista vacía de obj etiqueta
+			
+			/* listaEtiquetas = {"java", "programacion", "tecnología"}
+			 * 
+			 * tema = "java"
+			 * eti = null (Porque NO existe esa etiqueta)
+			 * nuevaEtiqueta = OBJ.tema = "java"
+			 * etiquetas = {Obj(java)}
+			 * 
+			 * tema = "programacion"
+			 * eti = Obj(programacion)
+			 * etiquetas = {Obj(java), Obj(programacion)}
+			 * 
+			 * tema = "tecnología"
+			 * eti = Obj(tecnologia)
+			 * etiquetas = {Obj(java), Obj(programacion), Obj(tecnologia)}
+			 */
+			for(String tema:listaEtiquetas) {
+				//Verificamos si el tema existe en las etiquetas de mi BD
+				tema = tema.trim();
+				
+				Etiqueta eti = servicio.encuentraEtiqueta(tema);
+				
+				if(eti == null) {
+					//No existe la etiqueta, la debemos de crear
+					Etiqueta nuevaEtiqueta = new Etiqueta();
+					nuevaEtiqueta.setTema(tema);
+					servicio.guardarEtiqueta(nuevaEtiqueta);
+					etiquetas.add(nuevaEtiqueta);
+				} else {
+					etiquetas.add(eti);
+				}
+			}
+			
+			pregunta.setEtiquetas(etiquetas);
+			servicio.guardarPregunta(pregunta);
+			return "redirect:/preguntas";
+			
+		}
+		
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/nueva.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>  
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Nueva Pregunta</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<h1>Nueva Pregunta</h1>
+		<!-- 
+		modelattribute -> OBJETO VACIO pregunta
+		pregunta.setContenido(INGRESO)
+		 -->
+		<form:form method="post" action="/crear" modelAttribute="pregunta" >
+			<div>
+				<form:label path="contenido">Pregunta</form:label>
+				<form:input path="contenido" class="form-control" />
+				<form:errors path="contenido" class="text-danger" />
+			</div>
+			<div>
+				<label>Etiquetas</label>
+				<input type="text" name="textoEtiquetas" class="form-control" />
+			</div>
+			<input type="submit" class="btn btn-success mt-3" value="Guardar" />
+		</form:form>
+	</div>
+</body>
+</html>
